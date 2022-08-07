e095a680b53caf16a1db6e9b2394010b2684dc07
Darren Ng <Un1Gfn@gmail.com>
2022-08-07 17:16:58 +0800

    missing fields in new_settings
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

--- bug.c
+++ bug.c
@@ -11,14 +11,44 @@
 #define FMT "a{sa{sv}}"
 #define PARSE "{'connection': {'autoconnect': <true>}}"
 
+void gv_check_print(GVariant *v){
 
-void gv_test(GVariant *v){
-  g_print("%s\n", g_variant_get_type_string(v));
-  g_print("%s\n", g_variant_print(v, /*type_annotate*/TRUE));
+  inline void gv_print(GVariant *v){
+    g_print("%s\n", g_variant_get_type_string(v));
+    g_print("\n");
+    g_print("%s\n", g_variant_print(v, /*type_annotate*/TRUE));
+    g_print("\n");
+  }
+
+  // check
+  g_assert_true(g_variant_check_format_string(v, FMT, TRUE));
+  g_assert_true(0==g_strcmp0(FMT, g_variant_get_type_string(v)));
+
+  // print
+  // g_print("%s\n", FMT);
+  // g_print("\n");
+  // g_print("%s\n", g_variant_print(v, TRUE));
+  // g_print("\n");
+  gv_print(v);
+
+  gsize sz=g_variant_get_size(v);
+  g_print("%lu\n", sz);
+  gchar data[sz+1]; bzero(data, sz+1);
+  g_variant_store(v, data);
+  g_assert_true(!isprint('\b'));
+  g_assert_true(!isprint('\v'));
+  g_assert_true(!isprint('\t'));
+  for(gsize i=0; i<sz; ++i){
+    gchar ch=data[i];
+    // g_print("[%lu] ", i);
+    putchar(isprint(ch)?ch:'.');
+  }
   g_print("\n");
+  g_print("\n");
+
 }
 
-GVariant *gv_new(){
+GVariant *gv_new_autoconnect_true(){
 
   // [ { s => [ s => v ] } ]
   // [{s => [s => v]}]
@@ -45,81 +75,54 @@ GVariant *gv_new(){
   g_variant_builder_add(b0, "{sa{sv}}", "connection", b1);
   vv=g_variant_new("a{sa{sv}}", b0); b0=NULL;
 
-  gv_test(vv);
+  gv_check_print(vv);
   g_assert_true(0==g_strcmp0(PARSE, g_variant_print(vv, /*type_annotate*/TRUE)));
 
   return vv;
 
 }
 
-void gv_print(GVariant *v){
-
-  // g_print("%s\n", );
-  g_assert_true(g_variant_check_format_string(v, FMT, TRUE));
-  g_assert_true(0==g_strcmp0(FMT, g_variant_get_type_string(v)));
-  g_print("%s\n", FMT);
-  g_print("\n");
-
-  g_print("%s\n", g_variant_print(v, TRUE));
-  g_print("\n");
-
-  gsize sz=g_variant_get_size(v);
-  g_print("%lu\n", sz);
-  gchar data[sz+1]; bzero(data, sz+1);
-  g_variant_store(v, data);
-  g_assert_true(!isprint('\b'));
-  g_assert_true(!isprint('\v'));
-  g_assert_true(!isprint('\t'));
-  for(gsize i=0; i<sz; ++i){
-    gchar ch=data[i];
-    // g_print("[%lu] ", i);
-    putchar(isprint(ch)?ch:'.');
-  }
-  g_print("\n");
-  g_print("\n");
-
-}
-
 int main(){
 
-  {
-    gv_new();
-    return 0;
-  }
+  GError *e=NULL;
+
+  // {
+  //   gv_new_autoconnect_true();
+  //   return 0;
+  // }
 
   g_print("\n");
   
   NMClient *client=nm_client_new(NULL, NULL); g_assert_true(client);
   g_print("NetworkManager version: %s\n", nm_client_get_version(client));
 
-  // Hotspot
+  // get connection "Hotspot"
   NMRemoteConnection *r_con=nm_client_get_connection_by_uuid(client, "22b985e1-e00e-4e0a-be92-8d9de9903968"); g_assert_true(r_con);
   NMConnection *con=NM_CONNECTION(r_con);
-
   NMSettingConnection *set_c=nm_connection_get_setting_connection(con); g_assert_true(set_c);
 
-  GVariant *v=nm_connection_to_dbus(con, NM_CONNECTION_SERIALIZE_ALL);
-  gv_print(v);
+  // list existing settings in all sections
+  // GVariant *v=nm_connection_to_dbus(con, NM_CONNECTION_SERIALIZE_ALL);
+  // gv_check_print(v);
 
-  gv_new();
-
-  // g_assert_true(NM_VARIANT_TYPE_CONNECTION==g_variant_get_type(v));
-  // GVariant *new_settings=g_variant_new(
-  // );
-  // GError *e=NULL;
-  // if(!nm_connection_replace_settings(con, new_settings, &e)){
-  //   g_critical("err: %s\n", e->message);
-  //   exit(0);
+  // replace settings
+  GVariant *new_settings=gv_new_autoconnect_true(); g_assert_true(new_settings);
+  // {
+  //   g_print("%s\n", g_variant_type_dup_string(NM_VARIANT_TYPE_SETTING));
+  //   g_print("%s\n", g_variant_type_dup_string(NM_VARIANT_TYPE_CONNECTION));
   // }
+  // g_assert_true(NM_VARIANT_TYPE_CONNECTION==g_variant_get_type(new_settings)); // pitfall!
+  g_assert_true(g_variant_type_equal(NM_VARIANT_TYPE_CONNECTION, g_variant_get_type(new_settings))); // correct
+  e=NULL; if(!nm_connection_replace_settings(con, new_settings, &e)){
+    g_critical("nm_connection_replace_settings() error: %s\n", e->message);
+    exit(0);
+  }
 
-  // alternative
-  // nm_connection_replace_settings()
-
-  // GError *e=NULL;
-  // if(!nm_remote_connection_commit_changes(r_con,TRUE,NULL,&e)){
-  //   g_critical("err: %s\n", e->message);
-  //   exit(0);
-  // }
+  // save to disk
+  e=NULL; if(!nm_remote_connection_commit_changes(r_con, TRUE, NULL, &e)){
+    g_critical("nm_remote_connection_commit_changes() error: %s\n", e->message);
+    exit(0);
+  }
 
   set_c=NULL;
   con=NULL;
